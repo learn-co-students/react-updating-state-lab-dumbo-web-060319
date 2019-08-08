c2971b09b5fdaaac795d3b8bc0e0ad6b1185fcad
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-08-08 10:34:41 -0400

    Done.

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,57 @@
 // Code YouTubeDebugger Component Here
+import React, { Component } from 'react';
+
+export default class YouTubeDebugger extends Component {
+    constructor () {
+        super();
+        this.state = {
+            errors: [],
+            user: null,
+            settings: {
+              bitrate: 8,
+              video: {
+                resolution: '1080p'
+              }
+            }
+          }
+ 
+       }
+
+    handleSettingsClick= () => {
+     this.setState({
+      settings: {
+        bitrate: 12,
+        video: {
+          resolution: '1080p'
+        }
+      }
+      })
+    }
+
+    handleResolutionClick = () => {
+      this.setState({
+        settings:{
+          bitrate: 8,
+          video: {
+            resolution: '720p'
+          }
+        }
+      })
+    }
+
+    render() {
+      return(
+        <div>
+          <button className="bitrate" onClick={this.handleSettingsClick}></button>
+          <button className="resolution" onClick={this.handleResolutionClick}></button>
+        </div>
+      )
+    }
+
+
+
+
+
+
+
+}
